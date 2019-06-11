9e6275f12311530016c3be9333e5a1b0b807723c
Derek Stobbs <dstobbs@googlemail.com>
2019-06-11 11:32:50 +0100

    Loony Lips: OK Button
    - Added an OK button as an alternative to pressing Return after the player has entered text

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -5,13 +5,19 @@ func _ready():
 	var story = "Once upon a time, %s watched %s and thought it was the %s movie of the past two decades"
 	
 	$VBoxContainer/DisplayText.text = story % words
-	$VBoxContainer/PlayerText.grab_focus()
+	$VBoxContainer/HBoxContainer/PlayerText.grab_focus()
 
 
 func _on_PlayerText_text_entered(new_text):
 	update_DisplayText(new_text)
 
 
+func _on_OKButton_pressed():
+	update_DisplayText($VBoxContainer/HBoxContainer/PlayerText.text)
+
+
 func update_DisplayText(new_text):
 	$VBoxContainer/DisplayText.text = new_text
-	$VBoxContainer/PlayerText.clear()
+	$VBoxContainer/HBoxContainer/PlayerText.clear()
+
+
