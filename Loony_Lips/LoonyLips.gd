4a3848689ba07b6b4f5562e0ee0ffb4ee1c7e911
Derek Stobbs <dstobbs@googlemail.com>
2019-06-11 10:29:11 +0100

    Loony Lips: Player Input
    - Player can now enter text, which will be copied to the display text when Return is pressed

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -4,4 +4,14 @@ func _ready():
 	var words = ["Yann", "Minions", "greatest"]
 	var story = "Once upon a time, %s watched %s and thought it was the %s movie of the past two decades"
 	
-	$DisplayText.text = story % words
+	$VBoxContainer/DisplayText.text = story % words
+	$VBoxContainer/PlayerText.grab_focus()
+
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+
+func update_DisplayText(new_text):
+	$VBoxContainer/DisplayText.text = new_text
+	$VBoxContainer/PlayerText.clear()
