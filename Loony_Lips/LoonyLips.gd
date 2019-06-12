981616948be2b09e1822264db582d9dc7c951bb6
Derek Stobbs <dstobbs@googlemail.com>
2019-06-12 11:17:41 +0100

    Loony Lips: Player prompts
    - Intro text now shown when first starting
    - Player is now prompted for each f the words to enter
    - Story displayed at the end once all words have been entered

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -1,23 +1,60 @@
 extends Control
 
+var intro ="Welcome to Loony Lips, a fun word game\n\n"
+var story = "Once upon a time, %s ate a %s sandwich, which made them feel all %s inside. It was a %s day"
+var prompts = ["a name", "a noun", "an adverb", "an adjective"]
+var player_words = []
+
+onready var DisplayText = $VBoxContainer/DisplayText
+onready var PlayerText =$VBoxContainer/HBoxContainer/PlayerText
+
 func _ready():
-	var words = ["Yann", "Minions", "greatest"]
-	var story = "Once upon a time, %s watched %s and thought it was the %s movie of the past two decades"
-	
-	$VBoxContainer/DisplayText.text = story % words
-	$VBoxContainer/HBoxContainer/PlayerText.grab_focus()
+	PlayerText.grab_focus()
+	display_intro_text()
+	check_player_words_length()
+
+
+func display_intro_text():
+	DisplayText.text = intro
 
 
 func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
+	add_to_player_words()
 
 
 func _on_OKButton_pressed():
-	update_DisplayText($VBoxContainer/HBoxContainer/PlayerText.text)
+	add_to_player_words()
 
 
-func update_DisplayText(new_text):
-	$VBoxContainer/DisplayText.text = new_text
-	$VBoxContainer/HBoxContainer/PlayerText.clear()
+func add_to_player_words():
+	player_words.append(PlayerText.text)
+	DisplayText.text = ""
+	PlayerText.clear()
+	check_player_words_length()
+
+
+func is_story_done():
+	return player_words.size() == prompts.size()
+
+
+func check_player_words_length():
+	if is_story_done():
+		tell_story()
+	else:
+		prompt_player()
+
+
+func tell_story():
+	DisplayText.text = story % player_words
+
+
+func prompt_player():
+	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
+
+
+
+
+
+
 
 
